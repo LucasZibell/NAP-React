65a880508015cfb6df1db5c2e82ddcb907b97182
lucaszibell <lucas.zibell@wolox.com.ar>
2019-10-07 02:13:14 -0300

    gs board

--- src/app/screens/ExerciseDetails/index.js
+++ src/app/screens/ExerciseDetails/index.js
@@ -7,13 +7,13 @@ import { actionCreators } from '@redux/ExerciseDetails/actions';
 
 class ExerciseDetails extends Component {
   componentDidMount() {
-    // this.props.getExerciseInfo(this.props.match.params.id);
-    const script = document.createElement('script');
+    this.props.getExerciseInfo(this.props.match.params.id);
+    // const script = document.createElement('script');
 
-    script.src = 'http://localhost:4000/assets/editor/editor.js';
-    script.async = true;
+    // script.src = 'http://localhost:9292/assets/editor/editor.js';
+    // script.async = true;
 
-    document.body.appendChild(script);
+    // document.body.appendChild(script);
   }
 
   render() {
@@ -21,11 +21,17 @@ class ExerciseDetails extends Component {
     return (
       <div className="column">
         <Text>Bienvenido al ejercicio</Text>
-        <link rel="stylesheet" href="http://localhost:4000/assets/editor/editor.css" />
-        {/* <script src="http://localhost:4000/assets/editor/editor.js" /> */}
-        <link rel="import" href="http://localhost:4000//assets/polymer.html" />
-        <link rel="import" href="http://localhost:4000/assets/gs-board.html" />
-        <link rel="import" href="http://localhost:4000/assets/editor/editor.html" />
+        <link rel="stylesheet" href="http://localhost:9292/assets/editor/editor.css" />
+        <script src="http://localhost:9292/assets/editor/editor.js" />
+        <link rel="import" href="http://localhost:9292/assets/polymer.html" />
+        <link rel="import" href="http://localhost:9292/assets/gs-board.html" />
+        <link rel="import" href="http://localhost:9292/assets/editor/editor.html" />
+        <div className="row space-between">
+          <gs-element-blockly />
+          <gs-board size='{ "x": 4, "y": 4 }' header='{ "x": 1, "y": 3 }' />
+          <gs-board size='{ "x": 4, "y": 4 }' header='{ "x": 1, "y": 1 }' />
+        </div>
+        <button className="kids-submit-button"> Mandar solucion</button>
         {loading ? (
           'Cargando...'
         ) : (
